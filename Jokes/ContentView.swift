b6e73d20a88c0d9eae7afa8d31abebb950bf008e
Ethan Lim <107737645+ethanolicacid@users.noreply.github.com>
2022-07-02 17:04:00 +0800

    Initial Commit

---
+++ Jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jokes
+//
+//  Created by Ethan Lim on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Ethan Lim on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
