963ff75cfab403ce9409cfe58e51513e1da05eea
Ethan Lim <ethanolicacid@outlook.com>
2022-07-09 11:17:59 +0800

    Added preview code

---
+++ Swift and SwiftUI/Swift Accelerator Programe/Codes/Friends/Shared/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Shared
+//
+//  Created by Ethan Lim on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
